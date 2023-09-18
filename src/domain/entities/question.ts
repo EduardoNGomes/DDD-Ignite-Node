091a01d852559dd1c4a4827fe3bccc7235a4f949
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-09-18 19:44:49 -0300

    mapeando realacionamentos

--- src/domain/entities/question.ts
+++ src/domain/entities/question.ts
@@ -1,14 +1,23 @@
 import { randomUUID } from 'node:crypto'
 
+interface QuestionProps {
+  title: string
+  content: string
+  authorId: string
+}
+
 export class Question {
   public id: string
 
   public title: string
   public content: string
 
-  constructor(title: string, content: string, id?: string) {
-    this.title = title
-    this.content = content
+  public authorId: string
+
+  constructor(props: QuestionProps, id?: string) {
+    this.title = props.title
+    this.content = props.content
+    this.authorId = props.authorId
     this.id = id ?? randomUUID()
   }
 }
