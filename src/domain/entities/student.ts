8e4e6645657a93fe71d0d9f1b76f26494772538e
Eduardo N Gomes <eduardo_n_gomes.dev@hotmail.com>
2023-09-18 15:58:12 -0300

    Primeiro caso de uso

---
+++ src/domain/entities/student.ts
@@ -0,0 +1,12 @@
+import { randomUUID } from 'node:crypto'
+
+export class Student {
+  public id: string
+
+  public name: string
+
+  constructor(name: string, id?: string) {
+    this.name = name
+    this.id = id ?? randomUUID()
+  }
+}
---
+++ src/domain/use-cases/answer-question.spec.ts
@@ -0,0 +1,14 @@
+import { expect, test } from 'vitest'
+import { AnswerQuestionUseCase } from './answer-question'
+
+test('create an answer', () => {
+  const answerQuestion = new AnswerQuestionUseCase()
+
+  const answer = answerQuestion.execute({
+    questionId: '1',
+    instructorId: '1',
+    content: 'Nova Resposta',
+  })
+
+  expect(answer.content).toEqual('Nova Resposta')
+})
